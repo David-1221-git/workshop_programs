234c9f1ecee192a4f49fc8fcab814f3abbc7b59a
David1221 <merinodominguezdavid@gmail.com>
2025-10-13 22:06:16 -0600

    test new repo

---
+++ DotNetInVSC/obj/Debug/netcoreapp3.0/.NETCoreApp,Version=v3.0.AssemblyAttributes.fs
@@ -0,0 +1,3 @@
+namespace Microsoft.BuildSettings
+                [<System.Runtime.Versioning.TargetFrameworkAttribute(".NETCoreApp,Version=v3.0", FrameworkDisplayName=".NET Core 3.0")>]
+                do ()
